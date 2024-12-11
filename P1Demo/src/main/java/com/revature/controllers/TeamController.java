7e3cce0fae1d120fb5ce3ab7e91dc1a399441888
benp3837 <petruzziellbj@alumni.vcu.edu>
2024-12-11 10:20:06 -0500

    controller now calls service, no error handling yet

--- P1Demo/src/main/java/com/revature/controllers/TeamController.java
+++ P1Demo/src/main/java/com/revature/controllers/TeamController.java
@@ -3,6 +3,8 @@ package com.revature.controllers;
 //The Controller layer handles HTTP Requests (sends back HTTP responses!)
 
 import com.revature.models.Team;
+import com.revature.services.TeamService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,15 +16,23 @@ import org.springframework.web.bind.annotation.RestController;
 //TODO: Add @CrossOrigin annotation to allow HTTP from anywhere
 public class TeamController {
 
-    //TODO: Add a service layer
+    //We're going to use constructor injection to dependency inject the Service
+    private TeamService teamService;
+
+    @Autowired
+    public TeamController(TeamService teamService) {
+        this.teamService = teamService;
+    }
 
     //Insert a new Team (any POST request ending in /teams will invoke this method)
     @PostMapping
     public ResponseEntity<Team> insertTeam(@RequestBody Team team) {
 
-        //TODO: send team to the service which will send it to the DAO
+        //Send team to the service which will send it to the DAO
+        Team newTeam = teamService.insertTeam(team);
 
-        return ResponseEntity.ok(team);
+        //Send back the returned Team object
+        return ResponseEntity.ok(newTeam);
 
         //ResponseEntity helps us build an HTTP Response
         //.ok() sets the status code to 200
