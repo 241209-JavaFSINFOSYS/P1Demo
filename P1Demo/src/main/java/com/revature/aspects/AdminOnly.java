9733e9dff8d66b72294fab5a7347a9240e1efda5
benp3837 <petruzziellbj@alumni.vcu.edu>
2024-12-18 10:30:30 -0500

    started Spring AOP stuff

---
+++ P1Demo/src/main/java/com/revature/aspects/AdminOnly.java
@@ -0,0 +1,12 @@
+package com.revature.aspects;
+
+//This is a custom annotation!
+//We can use it to annotate any controller method that can only be accessed by managers
+
+public @interface AdminOnly {
+
+    //No need for fields etc.
+
+    //TODO: some annotations
+
+}
---
+++ P1Demo/src/main/java/com/revature/aspects/AuthAspect.java
@@ -0,0 +1,18 @@
+package com.revature.aspects;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect //This Class is an ASPECT - a class that can trigger functionality at any point in our application
+//When a certain method is called, this class can listen for that, and trigger some method invocation
+@Component
+public class AuthAspect {
+
+    //2 use cases we'll see:
+        //1) When any method in the controller is called, we'll check if the user is logged in first
+            //We can bypass this check for login/register though
+        //2) When a method with our custom @AdminOnly annotation is called, check that the user is a manager
+
+
+
+}
